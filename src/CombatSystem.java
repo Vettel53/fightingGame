dc82283df416e789e1c430df662f410ba3b5d0a5
Javier <62152194+Vettel53@users.noreply.github.com>
2024-04-07 00:30:54 -0500

    JavaFX Implementation

--- src/CombatSystem.java
+++ src/CombatSystem.java
@@ -25,36 +25,37 @@ class enemyStats {
 
 }
 
-//class enemyStats {
-//    private String enemyName;
-//    private int enemyHealth;
-//    private int enemyDamage;
-//
-//    public enemyStats (String name, int health, int damage) {
-//        this.enemyName = name;
-//        this.enemyHealth = health;
-//        this.enemyDamage = damage;
-//    }
-//
-//    public String getName() {
-//        return enemyName;
-//    }
-//
-//    public int getHealth() {
-//        return enemyHealth;
-//    }
-//
-//    public int getDamage() {
-//        return enemyDamage;
-//    }
-//
-//}
+class playerStats {
+    private String playerName;
+    private int playerHealth;
+    private int playerDamage;
+
+    public playerStats (String name, int health, int damage) {
+        this.playerName = name;
+        this.playerHealth= health;
+        this.playerDamage = damage;
+    }
+
+    public String getName() {
+        return playerName;
+    }
+
+    public int getHealth() {
+        return playerHealth;
+    }
+
+    public int getDamage() {
+        return playerDamage;
+    }
+
+}
 
 public class CombatSystem {
 
     Scanner scnr = new Scanner(System.in);
 
     enemyStats ralphStats = new enemyStats("Ralph", 200, 50);
+    playerStats playerStats = new playerStats("Titan", 200, 50);
 
     public void startMenu() {
         int userInput;
@@ -82,11 +83,13 @@ public class CombatSystem {
     public void welcomeMessage() {
         System.out.println("\nWelcome to an early version of a math-battle game inspired by");
 
-        System.out.println(" ____  _____ ____ _____ ___ _   ___   __  ____");
-        System.out.println("|  _ \\| ____/ ___|_   _|_ _| \\ | \\ \\ / / |___ \\");
-        System.out.println("| | | |  _| \\___ \\ | |  | ||  \\| |\\ V /    __) |");
-        System.out.println("| |_| | |___ ___) || |  | || |\\  | | |    / __/");
-        System.out.println("|____/|_____|____/ |_| |___|_| \\_| |_|   |_____|");
+//        System.out.println(" ____  _____ ____ _____ ___ _   ___   __  ____");
+//        System.out.println("|  _ \\| ____/ ___|_   _|_ _| \\ | \\ \\ / / |___ \\");
+//        System.out.println("| | | |  _| \\___ \\ | |  | ||  \\| |\\ V /    __) |");
+//        System.out.println("| |_| | |___ ___) || |  | || |\\  | | |    / __/");
+//        System.out.println("|____/|_____|____/ |_| |___|_| \\_| |_|   |_____|");
+
+        combatStart(); // Start Combat
     }
 
     public void combatStart() {
@@ -95,7 +98,11 @@ public class CombatSystem {
         int enemyHealth = ralphStats.getHealth();
         int enemyDamage = ralphStats.getDamage();
 
-        //string playerName =
+        String playerName = playerStats.getName();
+        int playerHealth = playerStats.getHealth();
+        int playerDamage = playerStats.getDamage();
+
+
 
     }
 
