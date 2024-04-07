5e502c3699f31685de751eeb8e6cf894a2be10fe
Abram Ortiz <119538838+ksaii@users.noreply.github.com>
2024-04-07 18:04:45 -0500

    1st Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        new GameFrame();
         Enemy enemyRalph = new Ralph();
         Player playerNormal = new playerNormal();
         CombatSystem combatSystem = new CombatSystem();
