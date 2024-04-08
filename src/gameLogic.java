e81aac4bb90f441e3e67ccef01bc50f28c0b2a30
Abram Ortiz <119538838+ksaii@users.noreply.github.com>
2024-04-07 19:06:16 -0500

    1
    oops

--- src/gameLogic.java
+++ src/gameLogic.java
@@ -1,4 +1,56 @@
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Random;
+import java.awt.event.*;
+
+
+public class gameLogic extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 800; //Screen resolution Width ex: 1920x1080p tihs would be the 1920
+    static final int SCREEN_HEIGHT = 600;//Screen resolution Heigh ex: 1920x1080p tihs would be the 1080
+
+    static final int BLOCK_SIZE = 30; // Size of the UNIT/BLOCKS
+
+    static final int GAME_BLOCKS = (SCREEN_HEIGHT*SCREEN_WIDTH)/BLOCK_SIZE; //Amount of BLOCKS that will be in the Window/Screen
+
+    static final int TICKS = 60; //Refresh Rate the game will use to check and call methods
+
+    final int[] x = new int[GAME_BLOCKS];   //Array Holding the Main Character x position value
+    final int[] y = new int[GAME_BLOCKS];   //Array Holding the Main Character y position value
+
+
+    Random random;
+
+    gameLogic(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        //this.addKeyListener(new MyKeyAdapter()); this needs to be implemented still
+        startGame();
+    }
+
+    public void startGame(){
+
+    }
+
+
+
+
+
+    public void actionPerformed(ActionEvent e){
+        /*if(running){
+            checkMovement();
+            checkEnemy();
+            checkWall();            These are juse placeholder function names for functions that will be called every time the game is refreshed/tick so it will be checking if you moved and 
+            checkRoom();            then edit the x,y values of the character then call the repaint(); so that it updates the screen to move your character.
+        }
+        repaint();
+    */}
+
+}
+
+
 
 class enemyStats {
     private String enemyName;
@@ -50,7 +102,7 @@ class playerStats {
 
 }
 
-public class CombatSystem {
+class CombatSystem {
 
     Scanner scnr = new Scanner(System.in); // System scanner = scnr
     enemyGeneration enemyGen = new enemyGeneration(); // Object enemyGeneration.java
